459d83db6da867fea2750d3424cdd1cc48cacda1
Ray Kuo <ray20140811@gmail.com>
2020-10-09 22:32:59 +0800

    git.sh for update

---
+++ git.sh
@@ -0,0 +1,29 @@
+cd /mnt/d/Project/GitHub/JustForFun/
+git status
+git pull origin master
+cd ../
+
+cd /mnt/d/Project/GitHub/Parabola-101/
+git status
+git pull origin master
+cd ../
+
+cd /mnt/d/Project/GitHub/emacs-101/
+git status
+git pull origin master
+cd ../
+
+cd /mnt/d/Project/GitHub/GitHub-101/
+git status
+git pull origin master
+cd ../
+
+cd /mnt/d/Project/GitHub/JavaScript-101/
+git status
+git pull origin master
+cd ../
+
+cd /mnt/d/Project/GitHub/Bangle.js/
+git status
+git pull origin master
+cd ../
