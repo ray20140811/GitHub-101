430da940dcb6eefa165606a7ca6dcdf6b6fa565d
Linux User <ray@alpine.my.domain>
2020-10-13 00:34:38 +0800

    add gitclone.sh

---
+++ gitclone.sh
@@ -0,0 +1,5 @@
+#mkdir ~/Project
+#mkdir ~/Project/GitHub
+
+git clone https://github.com/ray20140811/JustForFun.git ~/Project/GitHub/JustForFun 
+git clone https://github.com/ray20140811/GitHub-101.git ~/Project/GitHub/GitHub-101
