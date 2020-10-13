123ae9f6f5e0c9caa4575574a0c6fdebf95c06cc
Ray Kuo <ray20200720@gmail.com>
2020-10-13 13:08:32 +0800

    add local_git_pull.sh

---
+++ local_git_pull.sh
@@ -0,0 +1,29 @@
+cd /mnt/d/Ray/MyProject/GitHub/20140811/JustForFun/
+git status
+git pull origin master
+cd ../
+
+cd /mnt/d/Ray/MyProject/GitHub/20140811/Parabola-101/
+git status
+git pull origin master
+cd ../
+
+cd /mnt/d/Ray/MyProject/GitHub/20140811/emacs-101/
+git status
+git pull origin master
+cd ../
+
+cd /mnt/d/Ray/MyProject/GitHub/20140811/GitHub-101/
+git status
+git pull origin master
+cd ../
+
+cd /mnt/d/Ray/MyProject/GitHub/20140811/JavaScript-101/
+git status
+git pull origin master
+cd ../
+
+cd /mnt/d/Ray/MyProject/GitHub/20140811/Bangle.js/
+git status
+git pull origin master
+cd ../
