fab606a5107e277f35739aebeb97d7dc7cddffaa
Ray Kuo <ray20200720@gmail.com>
2020-10-13 12:37:54 +0800

    rename

---
+++ git_clone_all_project.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#test ! -d ~/Project && mkdir ~/Project || echo "Project exist"
+#test ! -d ~/Project/GitHub && mkdir ~/Project/GitHub || echo "GitHub exist"
+#test ! -d ~/Project/GitHub/ray20140811 && mkdir ~/Project/GitHub/ray20140811 || echo "ray20140811 exist"
+
+test ! -d ~/Project/GitHub/ray20140811/JustForFun && git clone https://github.com/ray20140811/JustForFun.git ~/Project/GitHub/ray20140811/JustForFun || echo "JustForFun exist"
+
+test ! -d ~/Project/GitHub/ray20140811/GitHub-101 && git clone https://github.com/ray20140811/GitHub-101.git ~/Project/GitHub/ray20140811/GitHub-101 || echo "GitHub-101 exist"
+
+test ! -d ~/Project/GitHub/ray20140811/emacs-101 && git clone https://github.com/ray20140811/emacs-101.git ~/Project/GitHub/ray20140811/emacs-101 || echo "emacs-101 exist"
+
+test ! -d ~/Project/GitHub/ray20140811/JavaScript-101 && git clone https://github.com/ray20140811/JavaScript-101.git ~/Project/GitHub/ray20140811/JavaScript-101 || echo "JavaScript-101 exist"
+
+test ! -d ~/Project/GitHub/ray20140811/Parabola-101 && git clone https://github.com/ray20140811/Parabola-101.git ~/Project/GitHub/ray20140811/Parabola-101 || echo "Parabola-101 exist"
+
+test ! -d ~/Project/GitHub/ray20140811/Banglejs && git clone https://github.com/ray20140811/Banglejs.git ~/Project/GitHub/ray20140811/Banglejs || echo "Banglejs exist"
---
+++ git_pull_all_project.sh
@@ -0,0 +1,29 @@
+cd ~/Project/GitHub/ray20140811/JustForFun/
+git status
+git pull origin master
+cd ../
+
+cd ~/Project/GitHub/ray20140811/Parabola-101/
+git status
+git pull origin master
+cd ../
+
+cd ~/Project/GitHub/ray20140811/emacs-101/
+git status
+git pull origin master
+cd ../
+
+cd ~/Project/GitHub/ray20140811/GitHub-101/
+git status
+git pull origin master
+cd ../
+
+cd ~/Project/GitHub/ray20140811/JavaScript-101/
+git status
+git pull origin master
+cd ../
+
+cd ~/Project/GitHub/ray20140811/Banglejs/
+git status
+git pull origin master
+cd ../
