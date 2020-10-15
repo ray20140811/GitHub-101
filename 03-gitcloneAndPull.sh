b63353f33dfda29b2f04c07d6ef0d9da2abeb0bf
Ray Kuo <ray20200720@gmail.com>
2020-10-15 09:05:38 +0800

    update

---
+++ 03-gitcloneAndPull.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+test ! -d ~/Project/GitHub/ray20140811/JustForFun && git clone https://github.com/ray20140811/JustForFun.git ~/Project/GitHub/ray20140811/JustForFun || cd ~/Project/GitHub/ray20140811/JustForFun && git pull origin master
+
+test ! -d ~/Project/GitHub/ray20140811/GitHub-101 && git clone https://github.com/ray20140811/GitHub-101.git ~/Project/GitHub/ray20140811/GitHub-101 || cd ~/Project/GitHub/ray20140811/GitHub-101 && git pull origin master
+
+test ! -d ~/Project/GitHub/ray20140811/emacs-101 && git clone https://github.com/ray20140811/emacs-101.git ~/Project/GitHub/ray20140811/emacs-101 || cd ~/Project/GitHub/ray20140811/emacs-101 && git pull origin master
+
+test ! -d ~/Project/GitHub/ray20140811/JavaScript-101 && git clone https://github.com/ray20140811/JavaScript-101.git ~/Project/GitHub/ray20140811/JavaScript-101 || cd ~/Project/GitHub/ray20140811/JavaScript-101 && git pull origin master 
+
+test ! -d ~/Project/GitHub/ray20140811/Parabola-101 && git clone https://github.com/ray20140811/Parabola-101.git ~/Project/GitHub/ray20140811/Parabola-101 || cd ~/Project/GitHub/ray20140811/Parabola-101 && git pull origin master
+
+test ! -d ~/Project/GitHub/ray20140811/Banglejs && git clone https://github.com/ray20140811/Banglejs.git ~/Project/GitHub/ray20140811/Banglejs || cd ~/Project/GitHub/ray20140811/Banglejs && git pull origin master
