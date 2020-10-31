0709ddd74e1d3414ebec8a093e5d8d6f43b66097
Ray Kuo <ray20140811@gmail.com>
2020-10-31 08:55:54 +0800

    add FreeJapanese

--- 01-gitcloneAll.sh
+++ 01-gitcloneAll.sh
@@ -4,14 +4,17 @@
 #test ! -d ~/Project/GitHub && mkdir ~/Project/GitHub || echo "GitHub exist"
 #test ! -d ~/Project/GitHub/ray20140811 && mkdir ~/Project/GitHub/ray20140811 || echo "ray20140811 exist"
 
-test ! -d ~/Project/GitHub/ray20140811/JustForFun && git clone https://github.com/ray20140811/JustForFun.git ~/Project/GitHub/ray20140811/JustForFun || echo "JustForFun exist"
+test ! -d ~/Project/GitHub/ray20140811/JustForFun && git clone https://github.com/ray20140811/JustForFun.git ~/Project/GitHub/ray20140811/JustForFun || echo "JustForFun exist" && git pull origin master
 
-test ! -d ~/Project/GitHub/ray20140811/GitHub-101 && git clone https://github.com/ray20140811/GitHub-101.git ~/Project/GitHub/ray20140811/GitHub-101 || echo "GitHub-101 exist"
+test ! -d ~/Project/GitHub/ray20140811/GitHub-101 && git clone https://github.com/ray20140811/GitHub-101.git ~/Project/GitHub/ray20140811/GitHub-101 || echo "GitHub-101 exist" && git pull origin master
 
-test ! -d ~/Project/GitHub/ray20140811/emacs-101 && git clone https://github.com/ray20140811/emacs-101.git ~/Project/GitHub/ray20140811/emacs-101 || echo "emacs-101 exist"
+test ! -d ~/Project/GitHub/ray20140811/emacs-101 && git clone https://github.com/ray20140811/emacs-101.git ~/Project/GitHub/ray20140811/emacs-101 || echo "emacs-101 exist" && git pull origin master
 
-test ! -d ~/Project/GitHub/ray20140811/JavaScript-101 && git clone https://github.com/ray20140811/JavaScript-101.git ~/Project/GitHub/ray20140811/JavaScript-101 || echo "JavaScript-101 exist"
+test ! -d ~/Project/GitHub/ray20140811/JavaScript-101 && git clone https://github.com/ray20140811/JavaScript-101.git ~/Project/GitHub/ray20140811/JavaScript-101 || echo "JavaScript-101 exist" && git pull origin master
 
-test ! -d ~/Project/GitHub/ray20140811/Parabola-101 && git clone https://github.com/ray20140811/Parabola-101.git ~/Project/GitHub/ray20140811/Parabola-101 || echo "Parabola-101 exist"
+test ! -d ~/Project/GitHub/ray20140811/Parabola-101 && git clone https://github.com/ray20140811/Parabola-101.git ~/Project/GitHub/ray20140811/Parabola-101 || echo "Parabola-101 exist" && git pull origin master
 
-test ! -d ~/Project/GitHub/ray20140811/Banglejs && git clone https://github.com/ray20140811/Banglejs.git ~/Project/GitHub/ray20140811/Banglejs || echo "Banglejs exist"
+test ! -d ~/Project/GitHub/ray20140811/Banglejs && git clone https://github.com/ray20140811/Banglejs.git ~/Project/GitHub/ray20140811/Banglejs || echo "Banglejs exist" && git pull origin master
+
+test ! -d ~/Project/GitHub/ray20140811/FreeJapanese && git clone https://github.com/ray20140811/FreeJapanese.git ~/Project/GitHub/ray20140811/FreeJapanese || echo "FreeJapanese exist" && git pull origin master
+
