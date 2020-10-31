92fcbe663b5b673310d0d7eb959cb111bfaf32ca
Ray Kuo <ray20140811@gmail.com>
2020-10-31 09:03:29 +0800

    modify path

--- 02-gitpullAll.sh
+++ 02-gitpullAll.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-PHOME=~/Project/GitHub/ray20140811/
+PHOME=~/Project/GitHub/ray20140811
 #cd ~/Project/GitHub/ray20140811/JustForFun/
-cd $PHOME/JustForFun/
+cd $PHOME/JustForFun
 git status
 git pull origin master
 
 #cd ~/Project/GitHub/ray20140811/Parabola-101/
-cd $PHOME/Parabola-101/
+cd $PHOME/Parabola-101
 git status
 git pull origin master
 
 #cd ~/Project/GitHub/ray20140811/emacs-101/
-cd $PHOME/Parabola-101/
+cd $PHOME/emacs-101
 git status
 git pull origin master
 
 #cd ~/Project/GitHub/ray20140811/GitHub-101/
-cd $PHOME/GitHub-101/
+cd $PHOME/GitHub-101
 git status
 git pull origin master
 
 #cd ~/Project/GitHub/ray20140811/JavaScript-101/
-cd $PHOME/JavaScript-101/
+cd $PHOME/JavaScript-101
 git status
 git pull origin master
 
 #cd ~/Project/GitHub/ray20140811/Banglejs/
-cd $PHOME/Banglejs/
+cd $PHOME/Banglejs
 git status
 git pull origin master
